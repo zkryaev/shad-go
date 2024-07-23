e91bea5018e6a618408f17a999a2dbd1547ec49c
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2024-07-23 14:49:03 +0300

    Solved utf8

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,18 @@
 
 package reverse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func Reverse(input string) string {
-	return ""
+	var ans strings.Builder
+	ans.Grow(len(input))
+	for len(input) > 0 {
+		r, size := utf8.DecodeLastRuneInString(input)
+		ans.WriteRune(r)
+		input = input[:len(input)-size]
+	}
+	return ans.String()
 }
